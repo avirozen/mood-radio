2397e4d9123a449eb25274cccb21077a74d70343
avirozen <117524971+avirozen@users.noreply.github.com>
2022-12-22 10:25:37 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        basic.showIcon(IconNames.Happy)
+    } else if (receivedNumber == 1) {
+        basic.showIcon(IconNames.Sad)
+    } else {
+    	
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(0)
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(1)
+    basic.showIcon(IconNames.Sad)
+})
+radio.setGroup(1)
